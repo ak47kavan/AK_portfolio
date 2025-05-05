efd1d9fce9af53cdc015ea8232500c0c6184c182
A K Kavan <kavankulal2254@gmail.com>
2025-05-05 09:59:37 +0530

    ak_portfolio

---
+++ ak_website/src/App.jsx
@@ -0,0 +1,47 @@
+import { BrowserRouter } from "react-router-dom";
+
+import {
+  About,
+  Contact,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+  Footer,
+} from "./components";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="relative z-20 bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+          <StarsCanvas />
+        </div>
+        <div className="relative z-0">
+          <About />
+          <StarsCanvas />
+        </div>
+        <div className="relative z-0">
+          <Tech />
+          <Works />
+          <StarsCanvas />
+        </div>
+        <div className="relative z-0">
+          <Feedbacks />
+          <StarsCanvas />
+        </div>
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ ak_website/src/components/index.js
@@ -0,0 +1,27 @@
+import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas, TicofabCanvas } from './canvas';
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import About from "./About";
+import Tech from "./Tech";
+import Works from "./Works";
+import Feedbacks from "./Feedbacks";
+import Contact from "./Contact";
+import CanvasLoader from "./Loader";
+import Footer from "./Footer";
+
+export {
+  Hero,
+  Navbar,
+  About,
+  Tech,
+  Works,
+  Feedbacks,
+  Contact,
+  CanvasLoader,
+  EarthCanvas,
+  BallCanvas,
+  ComputersCanvas,
+  StarsCanvas,
+  Footer,
+  TicofabCanvas,
+};
